1d0eee4afc9034be470a1f4972b76607975999a7
ivankozak911 <kozkaindev@gmail.com>
2021-02-13 14:18:26 +0200

    add gitfiles

---
+++ gulpfile.js
@@ -0,0 +1,5 @@
+const gulp = require("gulp");
+const scss = require("gulp-sass");
+const minCss = require("gulp-csso");
+const minImg = require("gulp-imagemin");
+const rename = require("gulp-rename");
